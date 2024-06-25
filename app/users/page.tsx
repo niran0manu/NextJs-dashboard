af50f505ab0c5e031dbff753b4a4629308f26c96
niran0manu <niran0manu@gmail.com>
2024-06-25 09:29:43 +0300

    connected to the front-end

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,11 +1,37 @@
-import React from 'react'
+"use client";
+
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+
 
 const UsersPage = () => {
+  const [users, setUsers] = useState<any[]>([])
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const response = await axios.get('http://localhost:3000/api/users')
+        setUsers(response.data)
+        console.log(response.data)
+      } catch (error) {
+        console.error(error)
+      }
+    }
+
+    fetchUsers()
+  }, [])
+
   return (
     <div>
-      <p> all users</p>
+      <h1>All users</h1>
+      <ul>
+        {users.map((user: any) => (
+          <li key={user._id}>{user.username}--- {user.email}</li>
+        ))}
+      </ul>
     </div>
   )
 }
 
 export default UsersPage
+
